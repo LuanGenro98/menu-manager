534d989ef33185a93ef1781600d4fef7710a5a19
Gustavo Lamb <gustavo.lamb01@gmail.com>
2025-11-23 20:25:33 -0300

    Adicionando configuração para subir Frontend em instancia EC2

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -1,22 +1,21 @@
-"use client";
+'use client';
 
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 
 export function setToken(token: string) {
-  Cookies.set("token", token, {
+  Cookies.set('token', token, {
     expires: 7,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    path: "/",
+    secure: false,
+    sameSite: 'strict',
+    path: '/'
   });
 }
 
 export function getToken() {
-  return Cookies.get("token");
+  return Cookies.get('token');
 }
 
 export function logout() {
-  Cookies.remove("token");
-  window.location.href = "/login";
+  Cookies.remove('token');
+  window.location.href = '/login';
 }
-  
